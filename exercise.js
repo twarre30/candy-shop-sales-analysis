06339e12583cc48bf921baed218ea249b322983d
Tiffany <twarre30@gmail.com>
2022-03-25 15:15:22 -0500

    solution

--- exercise.js
+++ exercise.js
@@ -1,3 +1,31 @@
+function getTotalItemSales(sales, itemId){
+    return sales.filter(sale => sale.itemId === itemId)
+    .map(sale => sale.quantity)
+    .reduce((sum,number)=> sum + number,0)
+}
+
+function addTotalSalesToItems(sales, items) {
+    return items.map(item => {
+        return { ...item, 
+            quantity: getTotalItemSales(sales, item.id) }
+    })
+}
+
+function addTotalValueToItems(sales, items) {
+    return addTotalSalesToItems(sales, items).map(item => {
+        item.totalValue = (item.quantity * item.price).toFixed(2)
+        return item;
+    })
+}
+
+    
+
+
+
+
+
+
+
 module.exports = {
     // Uncomment these functions as you write them
     getTotalItemSales,
